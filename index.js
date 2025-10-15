9c8d88184deac0f06b76c88fcfd865ee3718d579
Finn Schmidt <finn.schmidt@brainworxx.de>
2025-10-15 12:30:58 +0200

    Buchobjektkonstruktor vertig

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { buchobjekt } from "./objektkonstruktor.js";
 
 let bibliotek = [];
 
-document.addEventListener("keydown", () => { // Achtung: war "keydownl", sollte "keydown" sein
+document.addEventListener("DOMContentLoaded", () => {
     const autor = "J.K. Rowling";
     const titel = "Der Hobbit";
     const seitenzahl = 320;
@@ -11,3 +11,13 @@ document.addEventListener("keydown", () => { // Achtung: war "keydownl", sollte 
     bibliotek.push(buch);
     console.log(bibliotek);
 });
+document.addEventListener("DOMContentLoaded", () => {
+    const autor = "Klaus";
+    const titel = "Harry Potter";
+    const seitenzahl = 504;
+    const id = crypto.randomUUID();
+    const buch = new buchobjekt(autor, titel, seitenzahl, id);
+    bibliotek.push(buch);
+    console.log(bibliotek);
+});
+
